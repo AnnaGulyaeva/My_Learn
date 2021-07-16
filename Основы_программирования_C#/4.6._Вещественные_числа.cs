13b6ff97845df5353a6a4300fa1fdfa7c166d04e
AnnaGulyaeva <83010587+AnnaGulyaeva@users.noreply.github.com>
2021-07-16 10:12:32 +0300

    Create 4.6._Вещественные_числа.cs

---
+++ Основы_программирования_C#/4.6._Вещественные_числа.cs
@@ -0,0 +1,19 @@
+// Дробная часть
+
+// Задача: дано положительное действительное число x x x. Вывести его дробную часть.
+
+// Входные данные: вводится положительное действительное число.
+
+// Выходные данные: вывести ответ на задачу.
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+            double x = Convert.ToDouble(Console.ReadLine());
+            int c = (int)x;
+            Console.WriteLine(x - c);
+    }
+}
