ace385fcba92a5bbe8dfca5ad1b90d172284981c
AnnaGulyaeva <83010587+AnnaGulyaeva@users.noreply.github.com>
2021-07-16 09:54:24 +0300

    Update 4.1._Целые_числа.cs

--- Основы_программирования_C#/4.1._Целые_числа.cs
+++ Основы_программирования_C#/4.1._Целые_числа.cs
@@ -1,1 +1,20 @@
+// Среднее арифметическое
 
+// Задача: даны два числа. Найти их среднее арифметическое.
+
+// Формат входных данных: на вход дается два целых положительных числа a a a и b b b, каждое из которых не превышает 10000 10000 10000.
+
+// Формат выходных данных: программа должна вывести среднее арифметическое чисел a a a и b b b.
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+            int а = Convert.ToInt32(Console.ReadLine());
+            int б = Convert.ToInt32(Console.ReadLine());
+            double с = (double)(а + б) / 2;
+            Console.WriteLine(с);
+    }
+}
